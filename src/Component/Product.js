d87cfe6fd4dca7b5830011f40569d3e3da7a27ed
Irwan333 <57896857+Irwan333@users.noreply.github.com>
2019-11-29 16:50:37 +0700

    upload 2.5

--- src/Component/Product.js
+++ src/Component/Product.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../Context';
+import storeProducts from '../data';
 
 class Product extends Component {
 	render() {
+		const {id,title,img,price,inCart} = this.props.Product;
 		return (
-			<div>
-				<h3> Ini Halaman Product </h3>
-			</div>
+			<ProductWarpper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+				<div className="card">
+					<div className="img-container p-5" onClick={() => console.log('clicked')}>
+						<Link to="/details">
+							<img src={img} alt="products" className="card-img-top"/>
+						</Link>
+						<button className="cart-btn" disable={inCart ? true : false}
+							onClick={() => {
+								console.log('Add to cart');
+							}} 
+						/>
+						{inCart ? (
+							<p className="text-capitalize mb-0" disable>{""}in inCart</p>
+						):(
+							<i className="fas fa-cart-plus"/>
+							)}
+					</div>
+				</div>
+			</ProductWarpper>
 		);
 	}
 }
 
 export default Product;
+
+const ProductWarpper = styled.div``;
--- src/Component/ProductList.js
+++ src/Component/ProductList.js
@@ -1,19 +1,26 @@
 import React, {Component} from 'react';
 import Product from './Product';
 import Title from './Title';
+import {ProductConsumer} from '../Context';
+import {storeProducts} from '../data';
 
 class ProductList extends Component {
-	state={
-		Products:[]
-	}
 
 	render() {
 		return (
 			<React.Fragment>
 				<div className="py-5">
 					<div className="container">
-					<Title name="our title="Products/>
+					<Title name="our" title="Products"/>
 					<div className="row">
+						<ProductConsumer>
+						{ value => {
+							{/*console.log(value)*/}
+							return value.products.map(product => {
+								return <Product key={product.id} Product={product}/>
+							})
+						}}
+						</ProductConsumer>
 					</div>
 					</div>
 				</div>
